1ef6f48bb4caabcd29d1dd50cb73cc22238338ce
alSheiye5h <anasbelkhadir13@gmail.com>
2025-09-12 21:59:02 +0100

    _

--- api/src/handlers/announcement/mod.rs
+++ api/src/handlers/announcement/mod.rs
@@ -1,11 +1,11 @@
-pub mod create;
-pub mod delete;
-pub mod get;
-pub mod list;
-pub mod update;
+pub mod create_announcement;
+pub mod delete_announcement;
+pub mod get_announcement;
+pub mod list_announcement;
+pub mod update_announcement;
 
-pub use create::create_announcement;
-pub use delete::delete_announcement;
-pub use get::get_announcement;
-pub use list::list_announcements;
-pub use update::update_announcement;
+pub use create_announcement::create_announcement;
+pub use delete_announcement::delete_announcement;
+pub use get_announcement::get_announcement;
+pub use list_announcement::list_announcements;
+pub use update_announcement::update_announcement;
