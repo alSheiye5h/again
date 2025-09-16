19e2d5d47d945ad333f3d368d13d22d8073b66e8
alSheiye5h <anasbelkhadir13@gmail.com>
2025-09-16 13:40:21 +0100

    _

---
+++ api/src/handlers/team/member/mod.rs
@@ -0,0 +1,4 @@
+pub mod add_member;
+pub mod delete_member;
+pub mod list_members;
+pub mod update_member;
---
+++ api/src/handlers/team/mod.rs
@@ -0,0 +1,5 @@
+pub mod create_team;
+pub mod delete_team;
+pub mod get_team;
+pub mod list_teams;
+pub mod update_team;
