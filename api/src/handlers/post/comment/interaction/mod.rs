fec052280e161eb018076ffc730cfa30ed9bca8f
alSheiye5h <anasbelkhadir13@gmail.com>
2025-09-18 23:40:54 +0100

    _

--- api/src/handlers/post/comment/interaction/mod.rs
+++ api/src/handlers/post/comment/interaction/mod.rs
@@ -1,8 +1,6 @@
 pub mod common;
 pub mod like_comment;
-pub mod remove_vote;
 pub mod unlike_comment;
 
 pub use like_comment::like_comment;
-pub use remove_vote::remove_vote;
 pub use unlike_comment::unlike_comment;
