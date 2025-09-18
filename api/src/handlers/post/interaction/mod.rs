33b5fe543430ed80464fdddd2b68a386ae02406c
alSheiye5h <anasbelkhadir13@gmail.com>
2025-09-18 18:49:31 +0100

    _

--- api/src/handlers/post/interaction/mod.rs
+++ api/src/handlers/post/interaction/mod.rs
@@ -1,15 +1,11 @@
 pub mod common;
 pub mod like_post;
-pub mod repost_post;
-pub mod share_post;
 pub mod unlike_post;
 pub mod upvote_post;
 pub mod downvote_post;
 pub mod remove_vote;
 
 pub use like_post::like_post;
-pub use repost_post::repost_post;
-pub use share_post::share_post;
 pub use unlike_post::unlike_post;
 pub use upvote_post::upvote_post;
 pub use downvote_post::downvote_post;
--- api/src/handlers/post/mod.rs
+++ api/src/handlers/post/mod.rs
@@ -4,3 +4,8 @@ pub mod list_posts;
 pub mod update_post;
 pub mod delete_post;
 pub mod interaction;
+pub mod repost_post;
+pub mod share_post;
+
+pub use repost_post::repost_post;
+pub use share_post::share_post;
